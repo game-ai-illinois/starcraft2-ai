b7cd5715a06d79391627c9c8053a81feb92f55c2
hyun14 <hyun14@illinois.edu>
2019-05-25 16:53:51 -0500

    changed titles

---
+++ Project/Policy/A2C/agent_a2c.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d0c115faf1434facc9eb60bdde44b34cb40e49b3c5fc0f68a28dc0af26df11a8
+size 4441
---
+++ Project/Policy/A2C/agent_a2c_simple.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9583410930d6b0b3217bff7774659f4cedc27982451923d91b671ff3f740444e
+size 4214
